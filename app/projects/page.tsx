71032cee0174790e1817dabb8f3420e07be796d3
Wint Phyo Paing <87934366+wppaing@users.noreply.github.com>
2025-02-09 23:03:52 +0630

    style: fix mobile responsive in project page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -30,7 +30,7 @@ export default function Projects() {
               <p className='prose prose-neutral dark:prose-invert pt-3'>
                 {project.description}
               </p>
-              <div className='flex gap-2 pt-2'>
+              <div className='flex flex-wrap gap-2 pt-2'>
                 {project.teachStack.map((tech) => (
                   <div
                     key={tech}
